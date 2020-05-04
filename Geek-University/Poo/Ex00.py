2f095bd536482005b3ce5dc1a4e39636a7eb7183
Italo.Neves <Magnavox789@github.com>
2020-05-04 10:10:46 -0300

    teoria

---
+++ Geek-University/Poo/Ex00.py
@@ -0,0 +1,37 @@
+"""
+Programação Orientada a objeto - POO
+
+- POO é um modelo de paradgma de programação que utiliza mapeamento
+de objetos do mundo real para modelos computaconais.
+
+- Paradgma de programação é a forma/metodologia utilizada para
+pensar/desenvolver sistemas.
+
+Principais elementos da orientação a orientação a objetos.
+- Classe -> Modelo do objeto do mundo real sendo representado computacionalmente.
+- Atributo -> Características dp objeto.
+- Método -> Comportamento do objeto(funções).
+- Construtor -> método especial utilizado para criar objeto.
+- Objeto -> instancia da classe.
+"""
+
+#--------------------------------------------------------------------------------------------------------------------
+"""
+POO - Classes 
+
+Em POO , classes nada mais são do que modelos dos objetos do mundo real sendo representados 
+computacionalmente.
+
+Classes podem conter: 
+    - Atributos -> Repesentam as cracteristicas dos objetos, ou seja , pelos atributos podemos representar 
+    computacionalmente os estados de um objeto. No caso da lâmpada, possivelmento iriamos querer saber se a
+    lâmpada é de 110 ou 220 volts, se ela é branca, amarela, vermelha, ou de iutra cor, qual é a luminosidade 
+    dela etc.
+    
+    - Métodos -> Representam os comportamentos do objeto, ou seja, as ações que este objeto pode realizar no seu
+    sistema. No caso da lampada por exemplo, um comportamento comum que muito provavelmento iriamos querer representar 
+    no nosso sistema é o de ligar e desliga a mesma.
+    
+Para definirmos uma classe me python usamos a palavra reservada 'class' 
+
+"""
