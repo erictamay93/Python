269b2fc64df7d6786b1578cdd85077d643731c0b
Italo.Neves <Magnavox789@github.com>
2020-05-09 22:19:47 -0300

    Objetos part01

--- Geek-University/Poo/PooObjetos.py
+++ Geek-University/Poo/PooObjetos.py
@@ -1,5 +1,20 @@
 """
 POO - Objetos
+# Objetos
+    -> São instancias de classe. ou seja, após o mapeamento do objeto real para a sua
+    representação computacional, devemos poder criar quantos objetos forem necessarios. Podemos pensar
+    os objetos/instancias de uma classe como variáveis do tipo definido da classe.
 
-Objetos
 """
+class Lampada:
+    def __init__(self, cor, voltagem, luminisidade):
+        self.__cor =cor
+        self._voltagem = voltagem
+        self.__luminosidade= luminisidade
+        self.__ligada = False
+
+
+class ContaCorrente:
+    contador = 4999
+
+    def __init__(self,limite, saldo):
